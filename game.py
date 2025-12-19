09389ed7de864a51476ead3aebea608987dbe881
flower1414000 <mariakhrist2007@gmail.com>
2025-12-19 15:23:45 +0700

    Add files via upload

---
+++ game.py
@@ -0,0 +1,24 @@
+import pygame
+import sys
+from config import *
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption(WINDOW_TITLE)
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            
+            self.screen.fill(BACKGROUND_COLOR)
+            pygame.display.flip()
+            self.clock.tick(FPS)
+        
+        pygame.quit()
+        sys.exit()
